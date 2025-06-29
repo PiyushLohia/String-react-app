145d7069f1fa6dc22bb38dcfb68f34fd9625d38c
Piyush Lohia <piyushlohia1857@gmail.com>
2025-06-29 15:22:55 +0530

    2.0

--- frontend/src/stateStore/authStore.js
+++ frontend/src/stateStore/authStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://string-react-app.onrender.com" || "http://localhost:5000";
 export const authStore = create((set,get) => ({
   authUser: null,
   isCheckingAuth: true,
