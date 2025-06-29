0344f4c8001ddb7f3c60f3bb40f295ed8074adc5
Piyush Lohia <piyushlohia1857@gmail.com>
2025-06-29 15:04:04 +0530

    2.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 const PORT = process.env.PORT;
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
 }
 ))
 app.use(cookieParser())
